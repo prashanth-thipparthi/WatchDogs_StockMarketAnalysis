823409bb6cad94c1396a6deb79bc5d12c8a6e7b1
prashanth thipparthi <prth3635@colorado.edu>
2019-04-24 17:49:34 +0000

    placing credentials in a config file

---
+++ twitter_config.py
@@ -0,0 +1,7 @@
+#These information comes from Twitter API
+#Create a Twitter Account and Get These Information from apps.twitter.com
+
+consumer_key =  'R1feyoEHAw2Lg32k4OKyyn01v'
+consumer_secret = 'KdxSi79P7PAjhmY4OYU2VfeKUcaUt4WmyJMb7Oew0nE1M7Ji5h'
+access_token = '145566333-KIndElco5MlHXPIGWeGkvjVXlMchLyQI2bMD8BJT'
+access_secret = 'iqfFlcM4clh6wCwDSLZaQqJttgstiIybUDOoB3uyzaZx8'
