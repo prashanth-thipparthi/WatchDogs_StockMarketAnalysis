7036d76d3db185ad2b4293c33086e65ab5be3f6c
kodur <chaitanyaradon89@gmail.com>
2019-05-04 16:56:59 -0600

    Updated Docker File

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,11 @@ docker tag flask_app gcr.io/vaulted-zodiac-236605/flask_api_server
 
 echo "docker build done"
 docker push gcr.io/vaulted-zodiac-236605/flask_api_server
-kubectl delete deployment.apps/api-deployment
 gcloud --quiet config set project "vaulted-zodiac-236605"
 gcloud --quiet config set compute/zone "us-central1-a"
 gcloud --quiet config set container/cluster "standard-cluster-1"
 gcloud --quiet container clusters get-credentials "standard-cluster-1"
+kubectl delete deployment.apps/api-deployment
 kubectl apply -f deploy.yml
 
 echo "script Complete"
