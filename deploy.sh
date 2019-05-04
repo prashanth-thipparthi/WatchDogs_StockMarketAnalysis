04fd1a502b3782b009d22e00595c1c337d0412d2
kodur <chaitanyaradon89@gmail.com>
2019-05-04 15:30:55 -0600

    Deploying to kubernetes

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 echo "running deploy script"
 set -e
 ls -l
-gcloud docker -a
+gcloud auth activate-service-account --key-file ./gcloud_project.json.json
+echo "Login success in gcloud."
+docker-credential-gcr configure-docker
 docker build -t flask_app .
 docker tag flask_app gcr.io/vaulted-zodiac-236605/flask_api_server
 
 echo "docker build done"
-gcloud auth activate-service-account --key-file ./My-project-key.json
 
-echo "Login success in gcloud."
 
 gcloud docker -- push gcr.io/vaulted-zodiac-236605/flask_api_server
 
