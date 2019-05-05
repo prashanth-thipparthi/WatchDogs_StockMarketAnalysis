7affb15e27c89a4534d70f36c4e6042f103d9341
kodur <chaitanyaradon89@gmail.com>
2019-05-04 19:30:25 -0600

    Added SIGTERM for k8

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from flask import abort
 from WatchDogs_RedisWrapper import RedisWrapper
+import sys
+import signal
+
+
+def handler(signal, frame):
+    my_logger.info('Flask App is being killed')
 
 app = Flask(__name__)
 
@@ -27,4 +33,8 @@ def get_polarity_tweets_of_stock(stock_name):
         abort(404)
 
 if __name__ == '__main__':
+    r = RedisWrapper()
+    my_logger = r.get_logger('Flask App')
+    my_logger.info('Flask API Server Started')
+    signal.signal(signal.SIGTERM, handler)
     app.run(host='0.0.0.0')
