33ac5df2a1c95de039d33f3af1db68de854d1911
kodur <chaitanyaradon89@gmail.com>
2019-05-04 21:46:07 -0600

    Added SIGTERM for k8

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import abort
 from WatchDogs_RedisWrapper import RedisWrapper
+from WatchDogs_MongoWrapper import MongoWrapper
+from bson.json_util import dumps
+import json
 import sys
 import signal
 
@@ -14,23 +17,48 @@ app = Flask(__name__)
 def it_works():
     return "Prashanth Rocks kafka!"
 
+# @app.route('/api/get_tweets_with_lat_long/<stock_name>')
+# def get_tweets_with_lat_long(stock_name):
+#     try:
+#         r = RedisWrapper()
+#         json_data = r.redis_get_json('get_tweets_with_lat_long/', stock_name)
+#         return json_data
+#     except:
+#         abort(404)
+#
+# @app.route('/api/get_polarity_tweets_of_stock/<stock_name>')
+# def get_polarity_tweets_of_stock(stock_name):
+#     try:
+#         r = RedisWrapper()
+#         json_data = r.redis_get_json('get_polarity_tweets_of_stock/', stock_name)
+#         return json_data
+#     except:
+#         abort(404)
+
 @app.route('/api/get_tweets_with_lat_long/<stock_name>')
 def get_tweets_with_lat_long(stock_name):
     try:
-        r = RedisWrapper()
-        json_data = r.redis_get_json('get_tweets_with_lat_long/', stock_name)
-        return json_data
+        mng = MongoWrapper()
+        data_frame = mng.get_tweets_with_lat_long(stock_name)
+        return data_frame.to_json()
     except:
         abort(404)
 
 @app.route('/api/get_polarity_tweets_of_stock/<stock_name>')
 def get_polarity_tweets_of_stock(stock_name):
+    mng = MongoWrapper()
     try:
-        r = RedisWrapper()
-        json_data = r.redis_get_json('get_polarity_tweets_of_stock/', stock_name)
-        return json_data
+        neg, neu, pos = mng.get_polarity_tweets_of_stock(stock_name)
+        neg_tweets = dumps(neg)
+        neu_tweets = dumps(neu)
+        pos_tweets = dumps(pos)
+        data = {"neg_tweets": neg_tweets, "neu_tweets": neu_tweets, "pos_tweets":pos_tweets  }
+        return json.dumps(data)
     except:
         abort(404)
+
+
+
 
 if __name__ == '__main__':
     r = RedisWrapper()
