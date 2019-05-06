7370e6884dd388473c9e2eb132690022d9af34d1
kodur <chaitanyaradon89@gmail.com>
2019-05-05 23:03:24 -0600

    Final Updates and bug fixes

--- populate_db.py
+++ populate_db.py
@@ -2,7 +2,6 @@ from kafka import KafkaConsumer
 from json import loads
 from WatchDogs_MongoWrapper import MongoWrapper
 from WatchDogs_RedisWrapper import RedisWrapper
-import traceback
 
 """
 Mongo_Group: The consumer is Mongo
@@ -13,38 +12,31 @@ if __name__ == "__main__":
     r = RedisWrapper()
     mng = MongoWrapper()
     test_logger = mng.get_logger('Kafka DB Populator')
-    try:
-        consumer = KafkaConsumer(
-            'mongoredis',
-             bootstrap_servers=['34.83.10.248:9092'],
-             auto_offset_reset='earliest',
-             enable_auto_commit=True,
-             group_id='MongoRedis',
-             value_deserializer=lambda x: loads(x.decode('utf-8')))
+    consumer = KafkaConsumer(
+        'mongoredis',
+         bootstrap_servers=['34.83.10.248:9092'],
+         auto_offset_reset='earliest',
+         enable_auto_commit=True,
+         group_id='MongoRedis',
+         value_deserializer=lambda x: loads(x.decode('utf-8')))
 
-        #### Pull all Companies and update the cache first
-        for each_company in mng.get_all_stocks():
-            stock_name = each_company['Company']
-            r.redis_update_json('get_tweets_with_lat_long/', stock_name)
-            r.redis_update_json('get_polarity_tweets_of_stock/', stock_name)
-        test_logger.info('Redis Cache Updated')
+    #### Pull all Companies and update the cache first
+    for each_company in mng.get_all_stocks():
+        stock_name = each_company['Company']
+        r.redis_update_json('get_tweets_with_lat_long/', stock_name)
+        r.redis_update_json('get_polarity_tweets_of_stock/', stock_name)
+    test_logger.info('Redis Cache Updated')
 
-        for message in consumer:
-            message_value = message.value
-            # print('message: ', message_value)
-            # test_logger = mng.get_logger('Kafka DB Populator')
-            # test_logger.info(message_value)
-            mng.insert_kafka_tweet_into_db(message_value, message_value['Search_Text'])
-            r.redis_insert_tweet(message_value['Search_Text'], message_value)
+    for message in consumer:
+        message_value = message.value
+        # print('message: ', message_value)
+        # test_logger = mng.get_logger('Kafka DB Populator')
+        # test_logger.info(message_value)
+        mng.insert_kafka_tweet_into_db(message_value, message_value['Search_Text'])
+        r.redis_insert_tweet(message_value['Search_Text'], message_value)
 
 
-    except Exception as e:
-        if e.__class__.__name__ == 'DuplicateKeyError':
-            pass
-        else:
-            print(traceback.format_exc())
-            test_logger.error(traceback.format_exc())
-            raise
+
 
 
 
